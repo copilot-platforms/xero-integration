bb22e5cb9ab66d7651804844c2f6dafa664f7338
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-12 17:17:33 +0545

    feat(OUT-2338): add sexy new logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,36 @@
+import util from 'node:util'
+
+type LogLevel = 'log' | 'info' | 'warn' | 'error'
+
+export interface Logger {
+  log: (...args: unknown[]) => void
+  info: (...args: unknown[]) => void
+  warn: (...args: unknown[]) => void
+  error: (...args: unknown[]) => void
+}
+
+const inspectOptions: util.InspectOptions = {
+  depth: null,
+  colors: Boolean(process.stdout.isTTY),
+}
+
+function formatArg(arg: unknown): string {
+  return typeof arg === 'string' ? arg : util.inspect(arg, inspectOptions)
+}
+
+function makeLogger(level: LogLevel): (...args: unknown[]) => void {
+  return (...args: unknown[]) => {
+    const line = args.map(formatArg).join(' ')
+    // biome-ignore lint/suspicious/noConsole: only 'log' level will be warned
+    console[level](line)
+  }
+}
+
+export const logger: Logger = {
+  log: makeLogger('log'),
+  info: makeLogger('info'),
+  warn: makeLogger('warn'),
+  error: makeLogger('error'),
+}
+
+export default logger
