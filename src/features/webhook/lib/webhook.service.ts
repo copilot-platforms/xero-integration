a180a5c7fcfc06f9986f76f5326f2b453ea51f86
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-19 12:16:07 +0545

    feat(OUT-2370): add webhook feature

---
+++ src/features/webhook/lib/webhook.service.ts
@@ -0,0 +1,59 @@
+import 'server-only'
+
+import XeroInvoiceSyncService from '@invoice-sync/lib/SyncedInvoices.service'
+import {
+  InvoiceCreatedEventSchema,
+  PriceCreatedEventSchema,
+  ProductUpdatedEventSchema,
+  ValidWebhookEvent,
+  type WebhookEvent,
+} from '@invoice-sync/types'
+import status from 'http-status'
+import APIError from '@/errors/APIError'
+import logger from '@/lib/logger'
+import AuthenticatedXeroService from '@/lib/xero/AuthenticatedXero.service'
+
+class WebhookService extends AuthenticatedXeroService {
+  async handleEvent(data: WebhookEvent) {
+    logger.info(
+      'WebhookService#handleEvent :: Handling webhook for user',
+      this.user.portalId,
+      this.user.token,
+    )
+    logger.info('WebhookService#handleEvent :: Received webhook event data', data)
+
+    const eventHandlerMap: Record<WebhookEvent['eventType'], (data: unknown) => Promise<object>> = {
+      [ValidWebhookEvent.InvoiceCreated]: this.handleInvoiceCreated,
+      [ValidWebhookEvent.ProductUpdated]: this.handleProductUpdated,
+      [ValidWebhookEvent.PriceCreated]: this.handlePriceCreated,
+    }
+    const handler = eventHandlerMap[data.eventType]
+    return await handler(data.data)
+  }
+
+  private handleInvoiceCreated = async (eventData: unknown) => {
+    const data = InvoiceCreatedEventSchema.parse(eventData)
+    if (data.status === 'draft') {
+      throw new APIError(`Ignoring draft invoice ${data.id}`, status.OK)
+    }
+
+    const xeroInvoiceSyncService = new XeroInvoiceSyncService(this.user, this.connection)
+    return await xeroInvoiceSyncService.syncInvoiceToXero(data)
+  }
+
+  private handleProductUpdated = async (eventData: unknown) => {
+    const data = ProductUpdatedEventSchema.parse(eventData)
+
+    await logger.info('WebhookService#handleProductUpdated :: Handling product updated event', data)
+    return {}
+  }
+
+  private handlePriceCreated = async (eventData: unknown) => {
+    const data = PriceCreatedEventSchema.parse(eventData)
+
+    await logger.info('WebhookService#handleProductUpdated :: Handling product updated event', data)
+    return {}
+  }
+}
+
+export default WebhookService
