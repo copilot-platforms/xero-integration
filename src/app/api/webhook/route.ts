f42b2976269af6d2af146af5047b110609c13fdd
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-23 16:24:38 +0545

    chore(OUT-2374): bump up maxDuration to 5 mins

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,4 +1,6 @@
 import { handleCopilotWebhook } from '@webhook/api/webhook.controller'
 import { withErrorHandler } from '@/utils/withErrorHandler'
 
+export const maxDuration = 300
+
 export const POST = withErrorHandler(handleCopilotWebhook)
