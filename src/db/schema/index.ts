bd8a44674edf03fcab72c7562832e984ffc936a7
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-23 14:39:17 +0545

    chore(OUT-2373): add support for query syntax

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,11 @@
+import { syncedContacts } from './syncedContacts.schema'
+import { syncedInvoices } from './syncedInvoices.schema'
+import { syncedItems } from './syncedItems.schema'
 import { xeroConnections } from './xeroConnections.schema'
 
 export const schema = {
   xeroConnections,
+  syncedContacts,
+  syncedInvoices,
+  syncedItems,
 }
