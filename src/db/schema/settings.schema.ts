4733120dfb40b5bce7fce7385f4603de7546d038
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-25 18:13:44 +0545

    feat(OUT-2416): add settings schema

---
+++ src/db/schema/settings.schema.ts
@@ -0,0 +1,33 @@
+import { boolean, pgTable, uniqueIndex, uuid, varchar } from 'drizzle-orm/pg-core'
+import { timestamps } from '@/db/db.helpers'
+
+export const settings = pgTable(
+  'settings',
+  {
+    id: uuid().primaryKey().notNull().defaultRandom(),
+
+    // Workspace ID / Portal ID in Copilot
+    portalId: varchar({ length: 16 }).notNull(),
+
+    // Active Tenant ID (most recently connected Xero organization)
+    tenantId: uuid().notNull(),
+
+    // Settings form checkbox flags
+    syncProductsAutomatically: boolean().notNull().default(false),
+    addAbsorbedFees: boolean().notNull().default(false),
+    useCompanyName: boolean().notNull().default(false),
+
+    // Whether or not sync is "Enabled" in this portal x tenantId
+    isSyncEnabled: boolean().notNull().default(false),
+
+    // Flags if user is mapping invoice settings for the first time
+    initialInvoiceSettingsMapping: boolean().notNull().default(false),
+
+    // Flags if user is mapping on the products table for the first time
+    initialProductSettingsMapping: boolean().notNull().default(false),
+
+    ...timestamps,
+  },
+  // Only allow one setting per portal x tenantId (each synced tenant must have a different setting)
+  (t) => [uniqueIndex('uq_settings_portal_id_tenant_id').on(t.portalId, t.tenantId)],
+)
