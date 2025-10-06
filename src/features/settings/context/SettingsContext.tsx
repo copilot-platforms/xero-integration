58f6c7c2d3e70559be40a8fd83a71b962df9908a
Rojan Rajbhandari <rrojan20@gmail.com>
2025-10-06 18:18:49 +0545

    feat(OUT-2416): add settings context and hook

---
+++ src/features/settings/context/SettingsContext.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { createContext, type ReactNode, useState } from 'react'
+
+type BaseSettingsContextType = {
+  syncProductsAutomatically: boolean
+  addAbsorbedFees: boolean
+  useCompanyName: boolean
+  initialInvoiceSettingsMapping: boolean
+  initialProductSettingsMapping: boolean
+}
+
+export type SettingsContextType = BaseSettingsContextType & {
+  initialSettings: BaseSettingsContextType
+}
+
+export const SettingsContext = createContext<
+  | (SettingsContextType & {
+      setSettings: React.Dispatch<React.SetStateAction<SettingsContextType>>
+      updateSettings: (state: Omit<Partial<SettingsContextType>, 'initialSettings'>) => void
+    })
+  | null
+>(null)
+
+export const SettingsContextProvider = ({
+  syncProductsAutomatically,
+  addAbsorbedFees,
+  useCompanyName,
+  initialInvoiceSettingsMapping,
+  initialProductSettingsMapping,
+  children,
+}: BaseSettingsContextType & { children: ReactNode }) => {
+  const [settings, setSettings] = useState<SettingsContextType>({
+    syncProductsAutomatically,
+    addAbsorbedFees,
+    useCompanyName,
+    initialInvoiceSettingsMapping,
+    initialProductSettingsMapping,
+    initialSettings: {
+      syncProductsAutomatically,
+      addAbsorbedFees,
+      useCompanyName,
+      initialInvoiceSettingsMapping,
+      initialProductSettingsMapping,
+    },
+  })
+
+  return (
+    <SettingsContext.Provider
+      value={{
+        ...settings,
+        setSettings,
+        updateSettings: (state) => {
+          setSettings({ ...settings, ...state })
+        },
+      }}
+    >
+      {children}
+    </SettingsContext.Provider>
+  )
+}
