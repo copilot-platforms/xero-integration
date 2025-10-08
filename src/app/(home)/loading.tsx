9a0f0e1b88aa64e477da30622a682e0fab0f0356
Rojan Rajbhandari <rrojan20@gmail.com>
2025-10-08 21:28:21 +0545

    fix(OUT-2417): loader has vertical offshift from assembly loader

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from '@/components/layouts/Loader'
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-2.5 py-10">
+    <div className="flex h-full flex-col items-center justify-center space-y-2.5 py-10">
       <Loader />
     </div>
   )
