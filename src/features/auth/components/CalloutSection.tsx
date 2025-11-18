49f91505b363885f7d391a52cd6ecef6871ad8dd
Rojan Rajbhandari <rrojan20@gmail.com>
2025-11-18 16:46:27 +0545

    feat(OUT-2635): add support for enable integration banner

--- src/features/auth/components/CalloutSection.tsx
+++ src/features/auth/components/CalloutSection.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import { useAuthContext } from '@auth/hooks/useAuth'
-import { Callout } from '@/components/ui/Callout'
+import { Callout } from 'copilot-design-system'
+import { updateSettingsAction } from '@/features/settings/actions/settings'
+import { useSettingsContext } from '@/features/settings/hooks/useSettings'
 
 export const CalloutSection = () => {
   const { user, connectionStatus } = useAuthContext()
+  const {
+    isSyncEnabled,
+    initialInvoiceSettingsMapping,
+    initialProductSettingsMapping,
+    initialSettings,
+    updateSettings,
+  } = useSettingsContext()
 
   if (!connectionStatus)
     return (
@@ -16,8 +25,34 @@ export const CalloutSection = () => {
           variant: 'primary',
           label: 'Connect to Xero',
           prefixIcon: 'Check',
+          onClick: (_e: unknown) => {
+            window.open(`/auth/initiate?token=${user.token}`, '_blank', 'noopener,noreferrer')
+          },
         }}
-        hrefUrl={`/auth/initiate?token=${user.token}`}
+      />
+    )
+
+  if (!isSyncEnabled)
+    return (
+      <Callout
+        title={'Confirm your mapping before getting started.'}
+        description={
+          'Set your product mappings and review configuration settings to best set up your sync.'
+        }
+        variant={'warning'}
+        actionProps={{
+          variant: 'primary',
+          label: 'Enable app',
+          prefixIcon: 'Check',
+          disabled: !(initialInvoiceSettingsMapping && initialProductSettingsMapping),
+          onClick: async (_e: unknown) => {
+            const newSettings = await updateSettingsAction(user.token, { isSyncEnabled: true })
+            updateSettings({
+              ...newSettings,
+              initialSettings: { ...initialSettings, ...newSettings },
+            })
+          },
+        }}
       />
     )
 
