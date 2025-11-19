c99fd3149ffaebdde43d9d6cc93f11fde9b19316
Rojan Rajbhandari <rrojan20@gmail.com>
2025-11-19 17:15:04 +0545

    docs(OUT-2641): clarify 4000 acc code

--- src/lib/xero/constants.ts
+++ src/lib/xero/constants.ts
@@ -1,7 +1,7 @@
 // Ref: https://www.xero.com/glossary/chart-of-accounts/
 // Ref: https://www.cubesoftware.com/blog/chart-of-accounts
 export enum AccountCode {
-  SALES = '4000', // This tracks income generated from business operations, like sales revenue
+  SALES = '4000', // This tracks income generated from business operations, like sales revenue for both products & services
 }
 
 // Ref: https://developer.xero.com/documentation/api/accounting/types#report-tax-types
