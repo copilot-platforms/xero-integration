3c865920b92bbdb2533052b4dce40917d9868d66
Rojan Rajbhandari <rrojan20@gmail.com>
2025-11-24 16:02:29 +0545

    feat(OUT-2674): ensure enablePaymentsToAccount is true for expense account

--- src/features/invoice-sync/lib/SyncedAccounts.service.ts
+++ src/features/invoice-sync/lib/SyncedAccounts.service.ts
@@ -1,8 +1,10 @@
 import status from 'http-status'
-import { type Account, AccountType } from 'xero-node'
+import type { Account } from 'xero-node'
+import z from 'zod'
 import APIError from '@/errors/APIError'
 import logger from '@/lib/logger'
 import AuthenticatedXeroService from '@/lib/xero/AuthenticatedXero.service'
+import { AccountCode } from '@/lib/xero/constants'
 
 class SyncedAccountsService extends AuthenticatedXeroService {
   async getOrCreateCopilotExpenseAccount(): Promise<Account> {
@@ -11,19 +13,36 @@ class SyncedAccountsService extends AuthenticatedXeroService {
     )
 
     const accounts = await this.xero.getAccounts(this.connection.tenantId)
-    let expenseAccount = accounts.find((acc) => acc.type === AccountType.EXPENSE)
-    if (!expenseAccount) {
-      expenseAccount = await this.xero.createExpenseAccount(this.connection.tenantId)
-      if (!expenseAccount) {
-        throw new APIError(
-          'Failed to create a new expense account in xero',
-          status.INTERNAL_SERVER_ERROR,
+    let expenseAccount = accounts.find((acc) => acc.code === AccountCode.MERCHANT_FEES)
+
+    // CASE I: Expense account exists
+    if (expenseAccount) {
+      if (!expenseAccount.enablePaymentsToAccount) {
+        // CASE II: Expense account exists but payments are disabled
+        await this.xero.makeAccountPaymentReceivable(
+          this.connection.tenantId,
+          z.string().parse(expenseAccount.accountID),
         )
       }
+      logger.info(
+        'SyncedAccountsService#getOrCreateCopilotExpenseAccount :: Created a new expense account:',
+        expenseAccount,
+      )
+
+      return expenseAccount
+    }
+
+    // CASE III: Expense account doesn't exist
+    expenseAccount = await this.xero.createExpenseAccount(this.connection.tenantId)
+    if (!expenseAccount) {
+      throw new APIError(
+        'Failed to create a new expense account in xero',
+        status.INTERNAL_SERVER_ERROR,
+      )
     }
 
     logger.info(
-      'SyncedAccountsService#getOrCreateCopilotExpenseAccount :: Found expense account:',
+      'SyncedAccountsService#getOrCreateCopilotExpenseAccount :: Created a new expense account:',
       expenseAccount,
     )
 
