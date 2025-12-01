be5244e79f232d8de5b8b49913b015d2defb266e
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-01 17:23:31 +0545

    chore(OUT-2632): add synclogs to service methods

--- src/errors/APIError.ts
+++ src/errors/APIError.ts
@@ -1,13 +1,20 @@
+import type { CreateSyncLogPayload } from '@/db/schema/syncLogs.schema'
 import { BaseServerError } from '@/errors/BaseServerError'
 
 class APIError extends BaseServerError {
+  readonly error?: unknown
+
   constructor(
     message: string,
-    public readonly status: number = 500,
-    public readonly error?: unknown,
+    readonly status: number = 500,
+    readonly opts?: {
+      error?: unknown
+      failedSyncLogPayload?: Omit<CreateSyncLogPayload, 'status' | 'syncDate' | 'errorMessage'>
+    },
   ) {
     super(message, status)
     this.name = 'APIError'
+    this.error = opts?.error
   }
 }
 
