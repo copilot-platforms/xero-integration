e3f24a458b19db97a5a88b68fb693d511de01ec1
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-08 19:34:39 +0545

    feat(OUT-2408): add cronjob to retry failed syncs

--- src/app/api/sync-logs/route.ts
+++ src/app/api/sync-logs/route.ts
@@ -1,6 +1,6 @@
 import { getSyncLogsCsv } from '@sync-logs/api/syncLogs.controller'
 import { withErrorHandler } from '@/utils/withErrorHandler'
 
-export const maxDuration = 300
+export const maxDuration = 800
 
 export const GET = withErrorHandler(getSyncLogsCsv)
